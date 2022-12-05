4723970ade93c6d914dca1f9c8b66d00282adf82
HaPhuong <phuongyuna520@gmail.com>
2022-12-05 08:30:27 +0700

    add splash screen

---
+++ cook_with_me/lib/pages/splash_screen/view/splash_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ForgotPasswordPage extends GetView<ForgotPasswordController> {
+  const ForgotPasswordPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
