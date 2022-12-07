b09dbb570202a7087d837412555dfbd276bbb48a
phuongtruong472001 <71766470+phuongtruong472001@users.noreply.github.com>
2022-12-07 15:14:27 +0700

    create model and setting screen

--- cook_with_me/lib/main.dart
+++ cook_with_me/lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:cook_with_me/pages/change_password/binding/change_password_binding.dart';
+import 'package:cook_with_me/pages/change_password/view/change_password_view.dart';
 import 'package:cook_with_me/pages/forgot_password/binding/forgot_password_binding.dart';
 import 'package:cook_with_me/pages/forgot_password/view/forgot_password_view.dart';
 import 'package:cook_with_me/pages/login_screen/login_view.dart';
+import 'package:cook_with_me/pages/settings/binding/settings_binding.dart';
+import 'package:cook_with_me/pages/settings/view/settings_view.dart';
 import 'package:cook_with_me/pages/splash_screen/binding/splash_binding.dart';
 import 'package:cook_with_me/pages/splash_screen/view/splash_view.dart';
 import 'package:flutter/material.dart';
@@ -23,14 +27,24 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/splash',
-      initialBinding: SplashBinding(),
+      initialRoute: '/settings',
+      initialBinding: SettingsBinding(),
       debugShowCheckedModeBanner: false,
       getPages: [
         GetPage(
           name: "/forgot_password",
           page: () => const ForgotPasswordPage(),
           binding: ForgotPasswordBinding(),
+        ),
+        GetPage(
+          name: "/change_password",
+          page: () => const ChangePasswordPage(),
+          binding: ChangePasswordBinding(),
+        ),
+        GetPage(
+          name: "/settings",
+          page: () => const SettingsPage(),
+          binding: SettingsBinding(),
         ),
         GetPage(
           name: "/login",
--- cook_with_me/lib/pages/change_password/view/change_password_view.dart
+++ cook_with_me/lib/pages/change_password/view/change_password_view.dart
@@ -1,14 +1,129 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hexcolor/hexcolor.dart';
+
 import '../controller/change_password_controller.dart';
 
 class ChangePasswordPage extends GetView<ChangePasswordController> {
-    const ChangePasswordPage({Key? key}) : super(key: key);
+  const ChangePasswordPage({Key? key}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(),
+      body: SafeArea(
+        child: Form(
+          key: controller.key,
+          child: Stack(
+            children: [
+              Positioned(
+                top: 20,
+                child: SizedBox(
+                  width: Get.width,
+                  height: 80,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.arrow_back_ios),
+                      ),
+                      const Text(
+                        "Change Password",
+                        style: TextStyle(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                top: Get.height / 4,
+                child: SizedBox(
+                  height: Get.height / 2,
+                  width: Get.width,
+                  child: Obx(() => Column(
+                        children: [
+                          editNewPassword(
+                              controller.newPasswordController.value, 1),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          editNewPassword(
+                              controller.confirmPasswordConTroller.value, 2),
+                        ],
+                      )),
+                ),
+              ),
+              Positioned(
+                bottom: Get.height / 6,
+                child: SizedBox(
+                  width: Get.width,
+                  child: Center(
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.black87,
+                        fixedSize: Size(Get.width - 32, 50),
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        ),
+                      ),
+                      onPressed: () {
+                        if (controller.key.currentState!.validate()) {
+                          controller.key.currentState!.save();
+                        }
+                      },
+                      child: const Text('Change password'),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(child: Container())
+            ],
+          ),
+        ),
+      ),
     );
-    }
+  }
+
+  Widget editNewPassword(TextEditingController emailController, int index) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 24, right: 24),
+          child: TextFormField(
+            controller: emailController,
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: HexColor("#000000"),
+              fontFamily: 'Nunito-Semibold',
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.w600,
+              fontSize: 18.0,
+            ),
+            decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                hintText:
+                    index == 1 ? "Enter your new password" : "Confirm password",
+                hintStyle: TextStyle(
+                  color: HexColor("#C1C1C1"),
+                  fontFamily: 'Nunito-Semibold',
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18.0,
+                ),
+                border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.circular(16))),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter the name.';
+              }
+              return null;
+            },
+          ),
+        ),
+      ],
+    );
+  }
 }
