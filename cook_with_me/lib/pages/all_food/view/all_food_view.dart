3f19a3b86d6119d701939f5b45369ce7495c478a
phuongtruong472001 <71766470+phuongtruong472001@users.noreply.github.com>
2022-12-09 02:04:54 +0700

    profile_view

--- cook_with_me/lib/pages/all_food/view/all_food_view.dart
+++ cook_with_me/lib/pages/all_food/view/all_food_view.dart
@@ -1,14 +1,134 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+import '../../../fonts_and_colors.dart';
 import '../controller/all_food_controller.dart';
 
 class AllFoodPage extends GetView<AllFoodController> {
-    const AllFoodPage({Key? key}) : super(key: key);
+  const AllFoodPage({Key? key}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.red,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const Text("Meomeo123"),
+                const SizedBox(
+                  height: 50,
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: HexColor("FA6163"),
+                    fixedSize: Size(Get.width - 100, 50),
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15.0),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    '+ ADD ITEM',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  itemCount: 2,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Container(
+                        height: 220,
+                        width: 150,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            boxShadow: const [
+                              BoxShadow(
+                                  offset: Offset(2, 4),
+                                  blurRadius: 10,
+                                  color: Color.fromRGBO(0, 0, 0, 0.25))
+                            ]),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 176,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(10),
+                                    topRight: Radius.circular(10)),
+                                color: Colors.red,
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Align(
+                                  alignment: Alignment.topRight,
+                                  child: GestureDetector(
+                                    child: const Icon(
+                                      Icons.more_vert,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 8, left: 8, right: 8),
+                              child: FittedBox(
+                                fit: BoxFit.fill,
+                                child: Text(
+                                  "Rainbow salad with salmon",
+                                  maxLines: 1,
+                                  style: TextStyle(
+                                    color: HexColor("437953"),
+                                    fontSize: 20,
+                                    fontFamily: FontsAndColors.semibold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
-    }
+  }
 }
