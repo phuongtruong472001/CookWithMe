05627b4ea55df2a6ac86a3011841e5162abf39f6
phuongtruong472001 <71766470+phuongtruong472001@users.noreply.github.com>
2022-12-09 11:09:00 +0700

    sign in and check otp

--- cook_with_me/lib/pages/sign_up/controller/sign_up_controller.dart
+++ cook_with_me/lib/pages/sign_up/controller/sign_up_controller.dart
@@ -1,18 +1,51 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SignUpController extends GetxController {
+  var emailController = TextEditingController().obs;
+  var newPasswordController = TextEditingController().obs;
+  var confirmPasswordConTroller = TextEditingController().obs;
+  var otpController = TextEditingController().obs;
+  final GlobalKey<FormState> key = GlobalKey<FormState>();
 
-    @override
-    void onInit() {
+  @override
+  void onInit() {
     super.onInit();
+  }
+
+  @override
+  void onReady() {}
+
+  @override
+  void onClose() {
+    emailController.value.dispose();
+    newPasswordController.value.dispose();
+    confirmPasswordConTroller.value.dispose();
+    super.onClose();
+  }
+
+  String validateEmail(String value) {
+    if (value.isEmpty || value == "") {
+      return "Not empty !";
+    }
+    if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+      return 'Định dạng email không đúng';
+    }
+    return "";
+  }
+
+  String validatePassword(String value) {
+    if (value.isEmpty || value == "") {
+      return "Not empty !";
     }
 
-    @override
-    void onReady() {  }
-
-    @override
-    void onClose() { 
-super.onClose(); 
- }
-
-   }
+    if (value.length < 8) {
+      return 'Password phải từ 8 kí tự trở lên';
+    }
+    if (!RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$')
+        .hasMatch(value)) {
+      return 'Password phải bao gồm: chữ hoa, thường, số và ký tự dặc biệt';
+    }
+    return "";
+  }
+}
